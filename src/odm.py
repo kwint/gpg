47c69841816471aa7837cf7215eb8a375f772325
Quinty van Dijk <quintvdijk@gmail.com>
2018-02-27 11:50:49 +0100

    Initial commit

---
+++ src/odm.py
@@ -0,0 +1,22 @@
+import rospy
+from std_msgs.msg import Int16
+import time
+
+
+def main():
+    rospy.init_node('motor_left_publisher')
+
+    pub_motor_left = rospy.Publisher('motor/dps/left', Int16, queue_size=1)
+
+    msg = Int16()
+    # dps 35 low, max 460
+    while(True):
+        msg.data = input("speed: ")
+        print(msg.data)
+        pub_motor_left.publish(msg)
+
+        time.sleep(0.5)
+
+
+if __name__ == '__main__':
+    main()
